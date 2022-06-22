c4107c8742ec7c1163d22bba606ee4aa843ad756
fsaleem32 <furkansaleem02@gmail.com>
2022-06-22 11:25:34 +0500

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,185 @@
+import image1 from "./assets/images/crowdfunding.png";
+import image2 from "./assets/images/dashboard.png";
+
+const logotext = "FURQAN";
+const meta = {
+  title: "Muhammad Furqan Saleem",
+  description:
+    "I’m Furqan Saleem Front-End Developer _ React Js devloper,currently working in OptimusFox Lahore",
+};
+
+const introdata = {
+  title: "I’m Furqan Saleem",
+  animated: {
+    first: "I am a Front-End Designer & Developer",
+    second: "I code cool websites",
+    third: "I develop mobile apps",
+  },
+  description:
+    "I'm a Front-end Web (React JS), Mobile (React Native) Developer. Who loves to write clean & simple code. I also enjoy learned new skills and applied them to daily tasks, improving efficiency and productivity.I Used critical thinking to break down problems, evaluate solutions and make decisions.",
+  your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+  title: "abit about my self",
+  aboutme:
+    "I'm a Front-end Web (React JS), Mobile (React Native) Developer. Who loves to write clean & simple code. I also enjoy learned new skills and applied them to daily tasks, improving efficiency and productivity.I Used critical thinking to break down problems, evaluate solutions and make decisions.",
+};
+const worktimeline = [
+  // {
+  //   jobtitle: "Designer of week",
+  //   where: "YAdfi",
+  //   date: "2020",
+  // },
+  // {
+  //   jobtitle: "Designer of week",
+  //   where: "Jamalya",
+  //   date: "2019",
+  // },
+  // {
+  //   jobtitle: "Designer of week",
+  //   where: "ALquds",
+  //   date: "2019",
+  // },
+];
+
+const skills = [
+  {
+    name: "Javascript",
+    value: 70,
+  },
+  {
+    name: "React",
+    value: 80,
+  },
+  {
+    name: "React-Native",
+    value: 70,
+  },
+  {
+    name: "Redux / Redux-thunk / Recoil",
+    value: 80,
+  },
+  {
+    name: "HTML , CSS",
+    value: 90,
+  },
+  {
+    name: "BootStrap / Material-Ui / Ant-D",
+    value: 80,
+  },
+];
+
+const services = [
+  {
+    title: "Web Apps",
+    description:
+      "I am a Professional web App (React js) designer and Developer having 2+ years of experiance. I have ability to convert figma design into web pages.I am capable to impliment API's to built in website.",
+  },
+  {
+    title: "Mobile Apps",
+    description:
+      "I am a Professional mobile App (React Native) designer and Developer having 1+ year of experiance. I have ability to convert figma design into mobile screens.I am capable to impliment API's to built in mobile app.",
+  },
+];
+
+const dataportfolio = [
+  {
+    img: "http://saphona.com.pk/subcatuploads/66619628-f5.jpg",
+    desctiption:
+      "A website that allows people to buy and sell female Dressess over the internet rather than at a brick-and-mortar location. ",
+    link: "http://saphona.com.pk/",
+  },
+  {
+    img: "https://picsum.photos/400/800/?grayscale",
+    desctiption: "Demo Pic.",
+    link: "#",
+    flag: "demo",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption: "Demo Pic.",
+    link: "#",
+    flag: "demo",
+  },
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    desctiption: "Demo Pic.",
+    link: "#",
+    flag: "demo",
+  },
+  {
+    img: image1,
+    desctiption: "An E-commerce website for client.",
+    link: "http://104.248.48.180:3000/",
+  },
+  {
+    img: image2,
+    desctiption: "An E-commerce website for client.",
+    link: "http://104.248.48.180:3000/",
+  },
+
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    desctiption: "Demo Pic.",
+    link: "#",
+    flag: "demo",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    desctiption: "Demo Pic.",
+    link: "#",
+    flag: "demo",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption: "Demo Pic.",
+    link: "#",
+    flag: "demo",
+  },
+  {
+    img: "https://picsum.photos/400/550/?grayscale",
+    desctiption: "Demo Pic.",
+    link: "#",
+    flag: "demo",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption: "Demo Pic.",
+    link: "#",
+    flag: "demo",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    desctiption: "Demo Pic.",
+    link: "#",
+    flag: "demo",
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "furkansaleem02@gmail.com",
+  YOUR_FONE: "(+92)-3006674004",
+  // description:
+  //   "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+  //   YOUR_SERVICE_ID: "service_id",
+  //   YOUR_TEMPLATE_ID: "template_id",
+  //   YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+  github: "https://github.com/fsaleem32",
+  linkedin: "https://www.linkedin.com/in/muhammad-furqan-057b6616a/",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
